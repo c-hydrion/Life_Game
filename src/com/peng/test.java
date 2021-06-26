006ec7a7253dc94e3d4358ec2d84e7b850f773a0
c-hydrion <623090599@qq.com>
2021-06-26 11:00:11 +0800

    lab7-1

---
+++ src/com/peng/test.java
@@ -0,0 +1,9 @@
+package com.peng;
+
+public class test {
+	public static void main(String[] args){
+        MainFrame m=new MainFrame();
+        m.setVisible(true);
+        m.setResizable(false);
+    }
+}
